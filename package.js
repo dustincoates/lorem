eebb33b13bf8da94a837afb7f58112e30a73dd8e
Dustin Coates <dustin@dcoates.com>
2015-07-30 23:53:55 -0400

    Add git repository

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'A lorem ipsum generator. Use straight ipsum or customize with your own words.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/dustincoates/lorem',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
